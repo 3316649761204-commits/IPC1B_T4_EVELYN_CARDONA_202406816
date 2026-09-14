55a1af65dd8feb1f0726c980aea50d812b1a1ec5
3316649761204-commits <3316649761204@ingenieria.usac.edu,gt>
2026-09-13 21:13:51 -0600

    main

---
+++ Curso/src/main/main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import view.MainFrame;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        MainFrame ventana = new MainFrame();
+
+        ventana.setVisible(true);
+    }
+}
